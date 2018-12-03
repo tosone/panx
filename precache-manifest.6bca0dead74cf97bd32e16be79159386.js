f0f935e5793e68fe419cb542e5329eebb3e12a48
Tosone <i@tosiney.com>
2018-12-03 23:25:04 +0800

    Updates

---
+++ precache-manifest.6bca0dead74cf97bd32e16be79159386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bf7f4293b514b8c678c",
+    "url": "/panx/static/css/main.b40d4788.chunk.css"
+  },
+  {
+    "revision": "8bf7f4293b514b8c678c",
+    "url": "/panx/static/js/main.8bf7f429.chunk.js"
+  },
+  {
+    "revision": "4575ba357564c55f4088",
+    "url": "/panx/static/css/1.075eac6b.chunk.css"
+  },
+  {
+    "revision": "4575ba357564c55f4088",
+    "url": "/panx/static/js/1.4575ba35.chunk.js"
+  },
+  {
+    "revision": "7a69c386695beaf0375f",
+    "url": "/panx/static/js/runtime~main.7a69c386.js"
+  },
+  {
+    "revision": "49adb432b5a51ef92875feefe5901c90",
+    "url": "/panx/static/media/card-bg.49adb432.png"
+  },
+  {
+    "revision": "61856d1998a64595f3cfc190e80ab863",
+    "url": "/panx/index.html"
+  }
+];
